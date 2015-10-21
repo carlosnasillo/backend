bbe872db31a4a839d62034e12d8743e92c13882a
Julien DERAY <julien@deray.fr>
2015-10-21 11:33:09 +0100

    implements liquidityByGrade

--- app/com/lattice/lib/integration/lc/impl/LendingClubReconciler.scala
+++ app/com/lattice/lib/integration/lc/impl/LendingClubReconciler.scala
@@ -7,9 +7,13 @@
  */
 package com.lattice.lib.integration.lc.impl
 
+
 import com.lattice.lib.integration.lc.{LendingClubConnection, LendingClubDb, LendingClubFactory}
 import com.lattice.lib.integration.lc.model.{LendingClubLoan, LendingClubNote, LoanListing, OrderPlaced}
 import com.lattice.lib.utils.Log
+import models.Grade
+import play.api.libs.json.JsValue
+import play.libs.Json
 
 /**
  * The reconciler is run periodically and reconciles the loans, notes, and accounts database with the state in lending club
@@ -43,10 +47,230 @@ class LendingClubReconciler(
     db.persistLoans(availableLoans)
     calculateLoanAnalytics(availableLoans)
   }
-
+  
   //TODO Julien - use this to calculate the analytics and persist the result to db
   private[impl] def calculateLoanAnalytics(loanListing: LoanListing) {
-    //TODO
+    val model: JsValue = Json.parse(
+      """
+        {
+          "asOfDate":"2015-10-20T05:58:49.506-07:00",
+          "loans":[
+              {
+                                "id" : 9712698,
+                                "memberId" : 11514201,
+                                "loanAmount" : 15000,
+                                "fundedAmount" : 1000,
+                                "term" : 36,
+                                "intRate" : 21.99,
+                                "expDefaultRate" : 10.55,
+                                "serviceFeeRate" : 1.11,
+                                "installment" : 572.78,
+                                "grade" : "E",
+                                "subGrade" : "E5",
+                                "empLength" : null,
+                                "homeOwnership" : "OWN",
+                                "annualInc" : 120000,
+                                "isIncV" : "NOT_VERIFIED",
+                                "acceptD" : "2015-10-15T20:03:48.000-07:00",
+                                "expD" : "2015-10-29T20:04:27.000-07:00",
+                                "listD" : "2015-10-15T20:04:27.000-07:00",
+                                "creditPullD" : "2015-10-15T20:03:45.000-07:00",
+                                "reviewStatusD" : null,
+                                "reviewStatus" : "NOT_APPROVED",
+                                "desc" : null,
+                                "purpose" : "house",
+                                "addrZip" : "110xx",
+                                "addrState" : "CA",
+                                "investorCount" : null,
+                                "ilsExpD" : "2015-11-01T11:04:51.000-08:00",
+                                "initialListStatus" : "F",
+                                "empTitle" : null,
+                                "accNowDelinq" : 0,
+                                "accOpenPast24Mths" : 23,
+                                "bcOpenToBuy" : 30000,
+                                "percentBcGt75" : 23,
+                                "bcUtil" : 23,
+                                "dti" : 0,
+                                "delinq2Yrs" : 1,
+                                "delinqAmnt" : 0,
+                                "earliestCrLine" : "1984-09-15T00:00:00.000-07:00",
+                                "ficoRangeLow" : 750,
+                                "ficoRangeHigh" : 754,
+                                "inqLast6Mths" : 0,
+                                "mthsSinceLastDelinq" : 90,
+                                "mthsSinceLastRecord" : 0,
+                                "mthsSinceRecentInq" : 14,
+                                "mthsSinceRecentRevolDelinq" : 23,
+                                "mthsSinceRecentBc" : 23,
+                                "mortAcc" : 23,
+                                "openAcc" : 3,
+                                "pubRec" : 0,
+                                "totalBalExMort" : 13944,
+                                "revolBal" : 1,
+                                "revolUtil" : 0,
+                                "totalBcLimit" : 23,
+                                "totalAcc" : 4,
+                                "totalIlHighCreditLimit" : 12,
+                                "numRevAccts" : 28,
+                                "mthsSinceRecentBcDlq" : 52,
+                                "pubRecBankruptcies" : 0,
+                                "numAcctsEver120Ppd" : 12,
+                                "chargeoffWithin12Mths" : 0,
+                                "collections12MthsExMed" : 0,
+                                "taxLiens" : 0,
+                                "mthsSinceLastMajorDerog" : 12,
+                                "numSats" : 8,
+                                "numTlOpPast12m" : 0,
+                                "moSinRcntTl" : 12,
+                                "totHiCredLim" : 12,
+                                "totCurBal" : 12,
+                                "avgCurBal" : 12,
+                                "numBcTl" : 12,
+                                "numActvBcTl" : 12,
+                                "numBcSats" : 7,
+                                "pctTlNvrDlq" : 12,
+                                "numTl90gDpd24m" : 12,
+                                "numTl30dpd" : 12,
+                                "numTl120dpd2m" : 12,
+                                "numIlTl" : 12,
+                                "moSinOldIlAcct" : 12,
+                                "numActvRevTl" : 12,
+                                "moSinOldRevTlOp" : 12,
+                                "moSinRcntRevTlOp" : 11,
+                                "totalRevHiLim" : 12,
+                                "numRevTlBalGt0" : 12,
+                                "numOpRevTl" : 12,
+                                "totCollAmt" : 12,
+                                "applicationType" : "INDIVIDUAL",
+                                "annualIncJoint" : null,
+                                "dtiJoint" : null,
+                                "isIncVJoint" : null
+                        },
+                        {
+                                "id" : 9712699,
+                                "memberId" : 11514202,
+                                "loanAmount" : 15000,
+                                "fundedAmount" : 0,
+                                "term" : 36,
+                                "intRate" : 21.99,
+                                "expDefaultRate" : 10.55,
+                                "serviceFeeRate" : 1.11,
+                                "installment" : 572.78,
+                                "grade" : "E",
+                                "subGrade" : "E5",
+                                "empLength" : null,
+                                "homeOwnership" : "OWN",
+                                "annualInc" : 120000,
+                                "isIncV" : "NOT_VERIFIED",
+                                "acceptD" : "2015-10-15T20:05:21.000-07:00",
+                                "expD" : "2015-10-29T20:06:04.000-07:00",
+                                "listD" : "2015-10-15T20:06:04.000-07:00",
+                                "creditPullD" : "2015-10-15T20:05:18.000-07:00",
+                                "reviewStatusD" : null,
+                                "reviewStatus" : "NOT_APPROVED",
+                                "desc" : null,
+                                "purpose" : "house",
+                                "addrZip" : "110xx",
+                                "addrState" : "CA",
+                                "investorCount" : null,
+                                "ilsExpD" : "2015-11-01T11:06:25.000-08:00",
+                                "initialListStatus" : "F",
+                                "empTitle" : null,
+                                "accNowDelinq" : 0,
+                                "accOpenPast24Mths" : 23,
+                                "bcOpenToBuy" : 30000,
+                                "percentBcGt75" : 23,
+                                "bcUtil" : 23,
+                                "dti" : 0,
+                                "delinq2Yrs" : 1,
+                                "delinqAmnt" : 0,
+                                "earliestCrLine" : "1984-09-15T00:00:00.000-07:00",
+                                "ficoRangeLow" : 750,
+                                "ficoRangeHigh" : 754,
+                                "inqLast6Mths" : 0,
+                                "mthsSinceLastDelinq" : 90,
+                                "mthsSinceLastRecord" : 0,
+                                "mthsSinceRecentInq" : 14,
+                                "mthsSinceRecentRevolDelinq" : 23,
+                                "mthsSinceRecentBc" : 23,
+                                "mortAcc" : 23,
+                                "openAcc" : 3,
+                                "pubRec" : 0,
+                                "totalBalExMort" : 13944,
+                                "revolBal" : 1,
+                                "revolUtil" : 0,
+                                "totalBcLimit" : 23,
+                                "totalAcc" : 4,
+                                "totalIlHighCreditLimit" : 12,
+                                "numRevAccts" : 28,
+                                "mthsSinceRecentBcDlq" : 52,
+                                "pubRecBankruptcies" : 0,
+                                "numAcctsEver120Ppd" : 12,
+                                "chargeoffWithin12Mths" : 0,
+                                "collections12MthsExMed" : 0,
+                                "taxLiens" : 0,
+                                "mthsSinceLastMajorDerog" : 12,
+                                "numSats" : 8,
+                                "numTlOpPast12m" : 0,
+                                "moSinRcntTl" : 12,
+                                "totHiCredLim" : 12,
+                                "totCurBal" : 12,
+                                "avgCurBal" : 12,
+                                "numBcTl" : 12,
+                                "numActvBcTl" : 12,
+                                "numBcSats" : 7,
+                                "pctTlNvrDlq" : 12,
+                                "numTl90gDpd24m" : 12,
+                                "numTl30dpd" : 12,
+                                "numTl120dpd2m" : 12,
+                                "numIlTl" : 12,
+                                "moSinOldIlAcct" : 12,
+                                "numActvRevTl" : 12,
+                                "moSinOldRevTlOp" : 12,
+                                "moSinRcntRevTlOp" : 11,
+                                "totalRevHiLim" : 12,
+                                "numRevTlBalGt0" : 12,
+                                "numOpRevTl" : 12,
+                                "totCollAmt" : 12,
+                                "applicationType" : "INDIVIDUAL",
+                                "annualIncJoint" : null,
+                                "dtiJoint" : null,
+                                "isIncVJoint" : null
+                        }
+                ]
+            }
+
+    val loanListingM = LendingClubConnectionImpl.availableLoans
+
+    val numLoans: Long = loanListingM.loans.size
+//    val liquidity: Long =
+    val numLoansByGrade: Map[Grade.Value, Long] = loanListingM.loans.groupBy(lcl => lcl.gradeEnum).mapValues(_.size)
+//    val liquidityByGrade: Map[Grade.Value, Long] = ???
+//    val dailyChangeInNumLoans: Double = ???
+//    val dailyChangeInLiquidity: Double = ???
+//    val loanOrigination: Map[LocalDate, Long] = ???
+//    val loanOriginationByGrade: Map[LocalDate, Map[Grade.Value, Long]] = ???
+//    val loanOriginationByYield: Map[LocalDate, Map[Double, Long]] = ???
+//    val originatedNotional: Map[LocalDate, Long] = ???
+//    val originatedNotionalByGrade: Map[LocalDate, Map[Grade.Value, Long]] = ???
+//    val originatedNotionalByYield: Map[LocalDate, Map[Double, Long]] = ???
+
+    println(numLoansByGrade)
+
+    /*LoanAnalytics(
+      numLoans,
+      liquidity,
+      numLoansByGrade,
+      liquidityByGrade,
+      dailyChangeInNumLoans,
+      dailyChangeInLiquidity,
+      loanOrigination,
+      loanOriginationByGrade,
+      loanOriginationByYield,
+      originatedNotional,
+      originatedNotionalByGrade,
+      originatedNotionalByYield
+    )*/
   }
 
   /**
@@ -111,4 +335,3 @@ class LendingClubReconciler(
   //TODO create the contract on bc and return its address
   def createLoanContract: String = ""
 
-}
